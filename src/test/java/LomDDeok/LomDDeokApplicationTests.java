5b80bab8dc1f45201c5dc6c3174eb27e55191179
sloth <slothfkdla@gmail.com>
2023-11-01 16:06:39 +0900

    초기 설정

---
+++ src/test/java/LomDDeok/LomDDeokApplicationTests.java
@@ -0,0 +1,13 @@
+package LomDDeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LomDDeokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
