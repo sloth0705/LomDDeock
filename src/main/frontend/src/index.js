56b3218e65a3e0f5e0830b9fbc40f770ef16a2d2
sloth <slothfkdla@gmail.com>
2023-11-02 14:05:03 +0900

    레이아웃 진행중

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-import Header from './layout/Header';
-import Footer from './layout/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header/>
-    <App />
-    <Footer/>
+    <App/>
   </React.StrictMode>
 );
 
