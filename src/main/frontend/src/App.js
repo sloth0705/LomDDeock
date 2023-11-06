98ef99a577abf2d7f4e53e42fc7f802aafd2cd79
sloth0705 <136422037+sloth0705@users.noreply.github.com>
2023-11-06 17:46:06 +0900

    진행중

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -11,6 +11,7 @@ const Register = lazy(() => import('./pages/user/Register'))
 const MenuList = lazy(() => import('./pages/menu/MenuList'))
 const MenuView = lazy(() => import('./pages/menu/MenuView'))
 const MenuCart = lazy(() => import('./pages/menu/MenuCart'))
+const MenuOrder = lazy(() => import('./pages/menu/MenuOrder'))
 
 function fallBackData() {
     <div>로딩중</div>
@@ -50,6 +51,10 @@ function App() {
                 <Route path="/menu/menuCart" element={
                 <Suspense fallback={fallBackData()}>
                     <MenuCart/>
+                </Suspense>} />
+                <Route path="/menu/menuOrder" element={
+                <Suspense fallback={fallBackData()}>
+                    <MenuOrder/>
                 </Suspense>} />
                 <Route path="/myInfo" element={
                 <Suspense fallback={fallBackData()}>
