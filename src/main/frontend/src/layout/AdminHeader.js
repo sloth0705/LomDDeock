6955edbc6acd778ad5e96ace9f4bd2ac8c7ffe12
oink24 <oooq3o@gmail.com>
2023-11-07 12:52:00 +0900

    adminHeader design

--- src/main/frontend/src/layout/AdminHeader.js
+++ src/main/frontend/src/layout/AdminHeader.js
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
-import logo from '../images/LomDDeock_logo.jpg';
+import logo from '../images/LomDDeock-letterlogo-korean.png';
 import '../css/layout.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function AdminHeader() {
     return (
         <header className="adminHeader">
-            <Link to="/"><img src={logo} alt="로고" /></Link>
-            <Link to="/admin/adminIndex"><div className="adminFontLogo">ADMIN</div></Link>
-            <div className="adminLogout"><a>로그아웃</a></div>
+            <div className="d-flex justify-content-center">
+                <Link to="/"><img src={logo} alt="로고" /></Link>
+                <Link to="/admin/adminIndex"><div className="adminFontLogo">ADMIN</div></Link>
+                <div className="adminLogout"><a>로그아웃</a></div>
+            </div>
         </header>
     )
 }
