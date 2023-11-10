2cea0a5dffe5a8f4aca70f7110f1ff1b628da86e
sloth0705 <136422037+sloth0705@users.noreply.github.com>
2023-11-10 14:48:11 +0900

    배포 설정 변경

---
+++ src/main/resources/static/static/js/988.effb97a8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[988],{9988:function(n,e,r){r.r(e);r(2791),r(8517),r(9717);var t=r(184);e.default=function(n){var e=n.children;return(0,t.jsx)(t.Fragment,{children:e})}}}]);
+//# sourceMappingURL=988.effb97a8.chunk.js.map
