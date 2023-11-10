9853eee697e156f5f2bbf9f25538dfd0de2ab58f
oink24 <oooq3o@gmail.com>
2023-11-10 12:34:48 +0900

    관리자 쿠폰지급내역 프론트작업

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -56,6 +56,7 @@ const AdminIndex = lazy(() => import('./pages/admin/AdminIndex'))
 const AdminMenuList = lazy(() => import('./pages/admin/AdminMenuList'))
 const AdminMenuRegister = lazy(()=>import('./pages/admin/AdminMenuRegister'))
 const AdminMenuModify = lazy(() => import('./pages/admin/AdminMenuModify'))
+const AdminCouponList = lazy(()=>import('./pages/admin/AdminCouponList'))
 
 /* 로딩중 표시 */
 function fallBackData() {
@@ -131,6 +132,7 @@ function App() {
                                 <Route path="/AdminMenuList" element={<AdminMenuList />} />
                                 <Route path="/AdminMenuRegister" element={<AdminMenuRegister />} />
                                 <Route path="/AdminMenuModify" element={<AdminMenuModify />} />
+                                <Route path="/AdminCouponList" element={<AdminCouponList />} />
                             </Routes>
                         </AdminLayout>
                         <Footer/>
