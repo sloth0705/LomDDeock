00332adf34476d3dc6fab80a92e99d555781a4e1
oink24 <oooq3o@gmail.com>
2023-11-13 02:02:11 +0900

    admin faq 프론트작업

--- src/main/frontend/src/pages/admin/faq/AdminFaqRegister.js
+++ src/main/frontend/src/pages/admin/faq/AdminFaqRegister.js
@@ -14,31 +14,37 @@ function adminFaqRegister() {
                     {/* aside menu */}
                     <AdminAsideMenu/>
                     <Col>
-                        <label>자주묻는질문 등록</label>
-                        {/* 유형선택 */}
-                        <InputGroup className="mb-3">
-                            <InputGroup.Text>유형선택<span>(필수)</span></InputGroup.Text>
-                            <Form.Select aria-label="유형선택">
-                                <option value="all">전체</option>
-                                <option value="event">이벤트</option>
-                                <option value="order">주문/결제</option>
-                                <option value="cancel">취소/환불</option>
-                                <option value="benefit">혜택</option>
-                                <option value="inquiry">이용문의</option>
-                                <option value="userInformation">회원정보</option>
-                                <option value="coupon">쿠폰</option>
-                            </Form.Select>
-                        </InputGroup>
-                        <InputGroup className="mb-3">
-                            <InputGroup.Text>질문<span>(필수)</span></InputGroup.Text>
-                            <Form.Control placeholder="질문을 입력해주세요."/>
-                        </InputGroup>
-                        <InputGroup className="mb-3">
-                            <InputGroup.Text>답변<span>(필수)</span></InputGroup.Text>
-                            <Form.Control as="textarea" aria-label="With textarea" placeholder="답변을 입력해주세요."/>
-                        </InputGroup>
-                        <Link to="/admin/faq/adminFaqRegister" className="btnCancel">취소</Link>
-                        <Link to="#" className="btnRegister">등록완료</Link>
+                        <div>
+                            <div>
+                                <h3>자주묻는질문 등록</h3>
+                            </div>
+                            {/* 유형선택 */}
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text>유형</InputGroup.Text>
+                                <Form.Select aria-label="유형선택">
+                                    <option value="all">전체</option>
+                                    <option value="event">이벤트</option>
+                                    <option value="order">주문/결제</option>
+                                    <option value="cancel">취소/환불</option>
+                                    <option value="benefit">혜택</option>
+                                    <option value="inquiry">이용문의</option>
+                                    <option value="userInformation">회원정보</option>
+                                    <option value="coupon">쿠폰</option>
+                                </Form.Select>
+                            </InputGroup>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text>질문</InputGroup.Text>
+                                <Form.Control placeholder="질문을 입력해주세요."/>
+                            </InputGroup>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text>답변</InputGroup.Text>
+                                <Form.Control as="textarea" aria-label="With textarea" placeholder="답변을 입력해주세요."/>
+                            </InputGroup>
+                            <div>
+                                <Link to="/admin/faq/adminFaqList" className="btnCancel">취소</Link>
+                                <Link to="#" className="btnRegister">등록완료</Link>
+                            </div>
+                        </div>
                     </Col>
                 </Row>
             </Container>
