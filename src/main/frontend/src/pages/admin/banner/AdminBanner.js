51ea76b32fd28d2c953f8a526c232109345054d1
oink24 <oooq3o@gmail.com>
2023-11-13 15:46:57 +0900

    admin banner 프론트작업

--- src/main/frontend/src/pages/admin/banner/AdminBanner.js
+++ src/main/frontend/src/pages/admin/banner/AdminBanner.js
@@ -2,6 +2,7 @@ import React from "react";
 import '../../../css/admin/admin.css';
 import {Col, Container, Row} from "react-bootstrap";
 import AdminAsideMenu from "../AdminAsideMenu";
+import FileUpload from "../fileUpload/FileUpload";
 
 function adminBanner() {
     return (
@@ -11,7 +12,21 @@ function adminBanner() {
                     {/* aside menu */}
                     <AdminAsideMenu/>
                     <Col>
-
+                        <div>
+                            <p>
+                                <label>배너1</label>
+                                <button>삭제</button>
+                            </p>
+                            <FileUpload/>
+                        </div>
+                        <div>
+                            <p>
+                                <label>배너2</label>
+                                <button>삭제</button>
+                            </p>
+                            <FileUpload/>
+                        </div>
+                        <button>완료</button>
                     </Col>
                 </Row>
             </Container>
